b3245698d960325adce0109d7c88f5dbfed92e3e
Matt Comins <mattcomins@gmail.com>
2022-04-02 15:54:27 -0500

    chore: create the foundation

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RecipesModule } from './recipes/recipes.module';
+
+@Module({
+  imports: [RecipesModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
