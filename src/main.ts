11648bb9f209eb9275d57fb195ee7e560c8eadc9
Matt Comins <mattcomins@gmail.com>
2022-04-19 14:31:07 -0500

    chore: add swagger api document builder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enables Automatic API Documentation-Building Using Decorators
+  const config = new DocumentBuilder()
+    .setTitle('Drift API')
+    .setDescription('An API for recipes')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
